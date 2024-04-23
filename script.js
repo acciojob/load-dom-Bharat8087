5ee3229a422957b8d23065cdf88477185966d0aa
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-23 14:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-    document.addEventListener('DOMContentLoaded', function() {
+   /* document.addEventListener('DOMContentLoaded', function() {
       document.body.innerHTML = ''; // Clear the body
 
       const paragraph = document.createElement('p'); // Create a paragraph element
       paragraph.textContent = 'DOM load success'; // Set the text of the paragraph
       document.body.appendChild(paragraph); // Append the paragraph to the body
     });
+*/
