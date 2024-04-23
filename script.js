54da7b3bc3a72b6bda476579032a5b4510f5a211
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-23 13:52:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// Function to execute when the DOM content is loaded
 function onDOMLoaded() {
-    if (document.body.childNodes.length === 0) {
+    if (document.body.childNodes.length === 0 || (document.body.childNodes.length === 1 && document.body.firstChild.tagName.toLowerCase() === 'script')) {
         const paragraph = document.createElement('p');
-        // Set the text content of the paragraph
         paragraph.textContent = 'DOM load success';
         document.body.appendChild(paragraph);
+    } else {
+        if (document.body.childNodes.length === 1 && document.body.firstChild.textContent === 'DOM load success') {
+            return;
+        } else {
+            document.body.innerHTML = '';
+            const paragraph = document.createElement('p');
+            paragraph.textContent = 'DOM load success';
+            document.body.appendChild(paragraph);
+        }
     }
 }
 
