7773d1827fada06cf9259c000da51578a6941fca
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-23 13:19:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    if (document.body.children.length === 0) {
-        const paragraph = document.createElement("p");
-        paragraph.textContent = "DOM load success";
-        document.body.appendChild(paragraph);
-    } else {
+    if (document.body.innerHTML !== '') {
         document.body.innerHTML = '';
-        const paragraph = document.createElement("p");
-        paragraph.textContent = "DOM load success";
-        document.body.appendChild(paragraph);
     }
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "DOM load success";
+    document.body.appendChild(paragraph);
 });
