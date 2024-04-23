01f649768cdb320d291a28ea7ee5164f755c1aad
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-23 13:58:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function onDOMLoaded() {
-    if (!document.body.firstChild || document.body.firstChild.tagName.toLowerCase() !== 'p') {
+    if (document.body.childNodes.length === 0 || (document.body.childNodes.length === 1 && document.body.firstChild.tagName.toLowerCase() === 'script')) {
         const paragraph = document.createElement('p');
         paragraph.textContent = 'DOM load success';
-        document.body.innerHTML = '';
         document.body.appendChild(paragraph);
+    } else {
+        if (document.body.childNodes.length === 1 && document.body.firstChild.textContent === 'DOM load success') {
+            return;
+        } else {
+            document.body.innerHTML = '';
+            const paragraph = document.createElement('p');
+            paragraph.textContent = 'DOM load success';
+            document.body.appendChild(paragraph);
+        }
     }
 }
 
