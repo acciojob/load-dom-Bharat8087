7af5e897967ba8d28e4b4adc287a6df0a67d0d98
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-23 13:06:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const paragraph = document.createElement("p");
     paragraph.textContent = "DOM load success";
     document.body.appendChild(paragraph);
+
+    if (document.body.children.length > 1) {
+        while (document.body.firstChild !== paragraph) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    }
 });
+
