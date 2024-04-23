f21641fa8f6954a72a3391c8088c7f2f64c6f492
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-23 13:03:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "DOM load success";
+    document.body.appendChild(paragraph);
+});
