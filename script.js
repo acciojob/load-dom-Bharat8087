2371e8495f1f13d62670f127664aaf0b0f729d64
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-23 13:46:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-window.onload = function() {
-    // Clear the entire body
-    document.body.innerHTML = '';
-
-    // Create a paragraph and append it to the body
-    const paragraph = document.createElement("p");
-    paragraph.textContent = "DOM load success";
-    document.body.appendChild(paragraph);
-};
+state: "failed",
+body: () => { 
+    cy.visit(baseUrl);
+    // Wait for the paragraph element to be added to the DOM
+    cy.get('p', { timeout: 4000 }).should('exist');
+    // After waiting, check for its absence
+    cy.get('p').should('not.exist');
+},
+displayError: "AssertionError: Timed out retrying after 4000ms: expected '<p>' not to exist in the DOM, but it was continuously found"
